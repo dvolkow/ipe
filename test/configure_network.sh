d0e5651745fa6352335f4fbb654c59b95a1fda2d
Daniel Wolkow <volkov12@rambler.ru>
2018-03-17 18:29:48 +0000

    [test] -- configure imp

--- test/configure_network.sh
+++ test/configure_network.sh
@@ -2,13 +2,19 @@
 set -x
 
 IP_PARENT='10.10.13.55/24'
-IP_CHILD='2.2.2.2/24'
+IP_CHILD1='2.2.2.2/24'
+IP_CHILD2='2.2.2.3/24'
 PARENT="enp3s0"
-CHILD="vlan1"
-VID=100
+CHILD1="vlan1"
+CHILD2="vlan2"
+VID1=100
+VID2=101
 
 ip addr add ${IP_PARENT} dev ${PARENT}
 
-ip link add link ${PARENT} name ${CHILD} 'type' vlan id ${VID}
-ip addr add ${IP_CHILD} dev ${CHILD}
-ip link 'set' ${CHILD} 'up'
+ip link add link ${PARENT} name ${CHILD1} 'type' vlan id ${VID1}
+ip link add link ${PARENT} name ${CHILD2} 'type' vlan id ${VID2}
+ip addr add ${IP_CHILD1} dev ${CHILD1}
+ip addr add ${IP_CHILD2} dev ${CHILD2}
+ip link 'set' ${CHILD1} 'up'
+ip link 'set' ${CHILD2} 'up'
