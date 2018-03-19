abbe1ac51f56b43096c111642b0cb10cf6fe7616
Daniel Wolkow <volkov12@rambler.ru>
2018-03-19 11:59:20 +0000

    [test] -- add sudo to command

--- test/configure_network.sh
+++ test/configure_network.sh
@@ -12,15 +12,15 @@ PARENTS=($PARENT $PARENT $DEF_PREFIX"1")
 IPS=('2.2.2.2/24' '2.2.2.3/24' '2.2.2.4/24')
 
 function set_IP {
-        ip addr add ${IP_PARENT} dev ${PARENT}
+        sudo ip addr add ${IP_PARENT} dev ${PARENT}
 }
 
 function add_links {
         for i in `seq 0 $COUNT`;
         do
-                ip link add link ${PARENTS[$i]} name ${DEF_PREFIX}${i} 'type' vlan id ${VIDS[$i]}
-                ip addr add ${IPS[$i]} dev ${DEF_PREFIX}${i}
-                ip link 'set' ${DEF_PREFIX}${i} 'up'
+                sudo ip link add link ${PARENTS[$i]} name ${DEF_PREFIX}${i} 'type' vlan id ${VIDS[$i]}
+                sudo ip addr add ${IPS[$i]} dev ${DEF_PREFIX}${i}
+                sudo ip link 'set' ${DEF_PREFIX}${i} 'up'
 
         done
 }
--- test/delete_ifs.sh
+++ test/delete_ifs.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 set -x
 
+
 COUNT=2
 UPPER="vlan3"
 
-ip link delete ${UPPER}
+sudo ip link delete ${UPPER}
 
 for i in `seq 0 $COUNT`;
 do 
-        ip link delete "vlan"${i}
+        sudo ip link delete "vlan"${i}
 done
